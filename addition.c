ef9f7780b60acfd4bed557130d45cfa6587e37f2
utsavsinha <uutsav@iitk.ac.in>
2013-11-08 14:40:30 +0530

    Create addition.c

---
+++ addition.c
@@ -0,0 +1,12 @@
+/* Program to add two numbers*/
+# include <stdio.h>   // Include Headers
+int main()  // Main function
+{
+	int a,b,c;  // Declares Vairables
+	scanf("%d", &a);  // Read 'a' from keyboard
+	scanf("%d", &b);  // Read 'b' from keyboard
+	c = a+b;
+	printf("%d\n",c); // Write 'c' to screen
+}
+
+
